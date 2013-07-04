98e4066e3a1a8c49adf8141a643a74997690728d
Ray <raymond@quandl.com>
2013-07-04 18:58:47 -0400

    Added new transformation

--- lib/quandl/operation/transform.rb
+++ lib/quandl/operation/transform.rb
@@ -23,7 +23,7 @@ class Transform
     end
     
     def valid_transformations
-      [ :diff, :rdiff, :cumul, :normalize ]
+      [ :diff, :rdiff, :cumul, :normalize, :rdiff_from ]
     end
   
     def transform( data, type)
@@ -103,6 +103,27 @@ class Transform
 
         #delete the first date in datapac (because there is no diff for that)
         data.delete_at(0)
+      elsif type == :rdiff_from
+        num_rows = keylist.length - 1 
+        initial = Array.new(numcols,nil)
+        num_rows.downto(1) do |i|
+          temparr[i] = []
+          curr_row = data[i][1..-1]
+          0.upto(numcols-1) do |x|
+            if curr_row[x].nil?
+              temparr[i][x] = nil
+            elsif initial[x].nil?
+              initial[x] = curr_row[i][x]
+              temparr[i][x] = 0.0
+            else
+              temparr[i][x] = ( Float(initial[x]) - Float(curr_row[x]) ) / Float(curr_row[x]) 
+          end
+        end
+
+
+        1.upto(keylist.length-1) do |i|
+          data[i] = [keylist[i], temparr[i]].flatten
+        end
       else
         cumulsum = Array.new(numcols,0)
         sumstarted = Array.new(numcols,false)
