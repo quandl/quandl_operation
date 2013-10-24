703f12385ad528916f138454ab3bd0e9c8f8537c
Blake Hilscher <blake@hilscher.ca>
2013-10-24 11:46:36 -0400

    add failing spec for collapse

--- spec/lib/quandl/operation/collapse_spec.rb
+++ spec/lib/quandl/operation/collapse_spec.rb
@@ -35,4 +35,23 @@ describe Quandl::Operation::Collapse do
     subject.collapses_greater_than_or_equal_to(:annual).should eq [:annual]
   end
   
+  describe "#perform" do
+  
+    let(:data){
+      [[2456537, 56.2, nil, nil], 
+       [2456518, 55.7, nil, nil], [2456506, nil, 136133.0, nil], 
+       [2456487, 55.4, nil, nil], [2456475, nil, 135964.0, nil], 
+       [2456457, 50.9, nil, nil], [2456445, nil, 135860.0, nil]]
+    }
+    
+    it "should collapse data with nils" do
+      subject.perform(data, :monthly).should eq [
+        [2456566, 56.2, nil, nil], 
+        [2456536, 55.7, 136133.0, nil], 
+        [2456505, 55.4, 135964.0, nil], 
+        [2456474, 50.9, 135860.0, nil]]
+    end
+  
+  end
+  
 end
