b539fd2834c79b5edb4581746473abb60af61170
Blake Hilscher <blake@hilscher.ca>
2013-10-30 17:45:12 -0400

    Release 0.1.14. -m

--- lib/quandl/operation/core_ext.rb
+++ lib/quandl/operation/core_ext.rb
@@ -1,4 +1,5 @@
 require 'quandl/operation/core_ext/array'
 require 'quandl/operation/core_ext/date'
-require 'quandl/operation/core_ext/time'
 require 'quandl/operation/core_ext/float'
+require 'quandl/operation/core_ext/string'
+require 'quandl/operation/core_ext/time'
