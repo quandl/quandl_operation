a56aaad62df7539b55a847cbafa903621e1360a7
Blake Hilscher <blake@hilscher.ca>
2013-11-22 18:15:29 -0500

    revise spec for qdformat

--- spec/lib/quandl/operation/qdformat_spec.rb
+++ spec/lib/quandl/operation/qdformat_spec.rb
@@ -4,32 +4,21 @@ require 'spec_helper'
 
 describe Quandl::Operation::QDFormat do
   
-  let(:full_code){ "SOURCE_CODE/DATASET_CODE" }
-  let(:name){ 'name: Test dataset name' }
-  let(:description){ 'description: Dataset description' }
-  let(:headers){ 'headers: Date, value, high, low' }
-  let(:data){ "2013-11-20,9.99470588235294,11.003235294117646,14.00164705882353\n2013-11-19,10.039388096885814,,14.09718770934256\n2013-11-18,10.03702588792184,11.040801329322205,14.148600982164867\n2013-11-16,10.019903902583621,10.99988541851354,14.186053161235304\n2013-11-15,9.98453953586862,10.922239168500502,\n2013-11-14,10.004508614940358,10.894612328250766,\n2013-11-13,,10.877309120435308,14.187437960548612\n2013-11-12,,10.838918617657301,14.22499294338536\n2013-11-11,9.965116185761039,10.827442115591547,14.178970907392053\n2013-11-09,9.881291973139637,10.924889094631869" }
-  
-  let(:output){
+  let(:qdf_dataset){
     %Q{
-      ---
-      #{full_code}
-      #{name}
-      #{description}
-      #{headers}
-      #{data}
-      -
-      - I am a comment.
-      -
-      #{full_code}
-      #{name}
-      #{description}
-      #{headers}
-      #{data}
-      -----
+      # YAML metadata
+      code:           DATASET_CODE
+      source_code:    SOURCE_CODE
+      name:           Test Dataset Name #{Time.now}
+      description:    "Here is a description with multiple lines.\n This is the second line."
+      # CSV data
+      Date, Value, High, Low
+      2013-11-20,9.99470588235294,11.003235294117646,14.00164705882353
+      2013-11-19,10.039388096885814,,14.09718770934256
     }
   }
-  let(:collection){ Quandl::Operation::QDFormat.parse(output) }
+  
+  let(:collection){ Quandl::Operation::QDFormat.parse(qdf_dataset) }
   
   describe ".parse" do
     
