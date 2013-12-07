fef311ba5b1ca54cc4b82fbefd45abe040d11a82
Blake Hilscher <blake@hilscher.ca>
2013-12-07 18:14:03 -0500

    value precision should ignore nils

--- lib/quandl/operation/value.rb
+++ lib/quandl/operation/value.rb
@@ -9,7 +9,7 @@ class Value
       data.each do |row|
         new_row = [row[0]]
         row[1..-1].each do |v|
-          new_row << Float("%.#{prec}g" % v)
+          new_row << (v.nil? ? v : Float("%.#{prec}g" % v))
         end
         r << new_row
       end
